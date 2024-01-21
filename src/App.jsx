3f3984402a322e7488f5d26096d24f1341f5c8b3
shahzaib vohra <m.shahzebvohra@outlook.com>
2024-01-20 19:16:22 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+    <> 
+      <div >
+        <a href="https://vitejs.dev " target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
